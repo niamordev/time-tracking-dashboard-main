1b24ae9d143dd5e97af8661ef98d4bbe56dd47e7
NiamorDev <115006018+niamordev@users.noreply.github.com>
2022-11-08 17:10:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -27,134 +27,166 @@ monthly.addEventListener('click', function(){
 
 
 
-// JS WITH JSON DATA 
 
-let http = new XMLHttpRequest();
-http.open('get', 'data.json', true);
-http.send();
-http.onload = function(){
-if(this.readyState == 4 && this.status == 200 ){
-    let products = JSON.parse(this.responseText);
-    let output = "";
+fetch("data.json")
+.then(function(response){
+   return response.json();
+})
 
+.then(function(products){
+   let placeholder = document.querySelector("#section-box-data");
+   let out = "";
+   for(let item of products){
 
+      out += `
+              <div class="box-data">
+              <div id="${item.id}" class="footer-box">
+              <img src="${item.img}" alt="${item.description}">
+              </div>
+              <div class="info-activity">
+              <div class="title-and-option">
+              <p class="title">${item.title}</p>
+              <img class="option-icon" src="images/icon-ellipsis.svg">
+              </div>
+              <div class="current-previous-div">
+              <p class="current-hour">${item.timeframes.daily.current}</p>
+              <p class="previous-hour">${item.timeframes.daily.previous}</p>
+              </div>
+              </div>
+              </div>
+              `;
 
-    for(let item of products){
-        if(daily.classList.contains('active')){
-        output += `
-        <div class="box-data">
-        <div id="${item.id}" class="footer-box">
-        <img src="${item.img}" alt="${item.description}">
-        </div>
-        <div class="info-activity">
-        <div class="title-and-option">
-        <p class="title">${item.title}</p>
-        <img src="images/icon-ellipsis.svg">
-        </div>
-        <div class="current-previous-div">
-        <p class="current-hour">${item.timeframes.daily.current}</p>
-        <p class="previous-hour">${item.timeframes.daily.previous}</p>
-        </div>
-        </div>
-        </div>
-        `;
-        console.log('daily')
-        
-    }  
-
-    if(weekly.classList.contains('active')){
-        output += `
-        <div class="box-data">
-        <div id="${item.id}" class="footer-box">
-        <img src="${item.img}" alt="${item.description}">
-        </div>
-        <div class="info-activity">
-        <div class="title-and-option">
-        <p class="title">${item.title}</p>
-        <img src="images/icon-ellipsis.svg">
-        </div>
-        <p class="current-hour">${item.timeframes.weekly.current}</p>
-        <p class="previous-hour">${item.timeframes.weekly.previous}</p>
-        </p>
-        </div>
-        </div>
-        `;
-        console.log('weekly')
-       
-    }   
-
-    if(monthly.classList.contains('active')){
-        output += `
-        <div class="box-data">
-        <div id="${item.id}" class="footer-box">
-        <img src="${item.img}" alt="${item.description}">
-        </div>
-        <div class="info-activity">
-        <div class="title-and-option">
-        <p class="title">${item.title}</p>
-        <img src="images/icon-ellipsis.svg">
-        </div>
-        <p class="current-hour">${item.timeframes.monthly.current}</p>
-        <p class="previous-hour">${item.timeframes.monthly.previous}</p>
-        </p>
-        </div>
-        </div>
-        `;
-        console.log('monthly')
-        
-    } 
+   }
+ 
+   placeholder.innerHTML = out;
+});
 
 
-}
-document.querySelector(".section-box-data").innerHTML = output;
+daily.addEventListener('click', function(){
+fetch("data.json")
+.then(function(response){
+   return response.json();
+})
 
-}
-}
+.then(function(products){
+   let placeholder = document.querySelector("#section-box-data");
+   let out = "";
+   for(let item of products){
 
+      out += `
+              <div class="box-data">
+              <div id="${item.id}" class="footer-box">
+              <img src="${item.img}" alt="${item.description}">
+              </div>
+              <div class="info-activity">
+              <div class="title-and-option">
+              <p class="title">${item.title}</p>
+              <img src="images/icon-ellipsis.svg">
+              </div>
+              <div class="current-previous-div">
+              <p class="current-hour">${item.timeframes.daily.current}</p>
+              <p class="previous-hour">${item.timeframes.daily.previous}</p>
+              </div>
+              </div>
+              </div>
+              `;
 
-// document.querySelector('body').addEventListener('click', function(){
-// 	if(urlcourante == 'http://127.0.0.1:5500/html/controllers.html' ){
-//         alert('tt')
-// 	}
-		
-// 		else{
-// 			alert(urlcourante)
-// 		}
-	
-// })
-	
+   }
+ 
+   placeholder.innerHTML = out;
+});
+
+})
 
 
 
 
-// let http = new XMLHttpRequest();
-// http.open('get', 'data.json', true);
-// http.send();
-// http.onload = function(){
-// if(this.readyState == 4 && this.status == 200 ){
-//     let products = JSON.parse(this.responseText);
-//     let output = "";
 
 
-//     for(let item of products){
-//         output += `
-//         <div class="products"> 
-//       <div class="tag-work"><img src="${item.img}" alt=""></div>
-//       <div class="data-box">
-//         <div class="activity-and-option">
-//           <span class="activity">${item.title}</span>
-//           <img src="images/icon-ellipsis.svg" alt="">
-//         </div>
-//         <span class="current-hour">${item.current}</span>
-//         <span class="previous-hour">${item.previous}</span>
-//       </div>
-//         `;
-//     }  
+
+
+
+
+
+weekly.addEventListener('click', function(){
+    fetch("data.json")
+    .then(function(response){
+       return response.json();
+    })
     
+    .then(function(products){
+       let placeholder = document.querySelector("#section-box-data");
+       let out = "";
+       for(let item of products){
+    
+          out += `
+                  <div class="box-data">
+                  <div id="${item.id}" class="footer-box">
+                  <img src="${item.img}" alt="${item.description}">
+                  </div>
+                  <div class="info-activity">
+                  <div class="title-and-option">
+                  <p class="title">${item.title}</p>
+                  <img src="images/icon-ellipsis.svg">
+                  </div>
+                  <div class="current-previous-div">
+                  <p class="current-hour">${item.timeframes.weekly.current}</p>
+                  <p class="previous-hour">${item.timeframes.weekly.previous}</p>
+                  </div>
+                  </div>
+                  </div>
+                  `;
+    
+       }
+     
+       placeholder.innerHTML = out;
+    });
+    
+    })
 
 
 
-// document.querySelector(".products").innerHTML = output;
 
-//   }
-// }
+
+
+
+
+
+
+
+
+    monthly.addEventListener('click', function(){
+        fetch("data.json")
+        .then(function(response){
+           return response.json();
+        })
+        
+        .then(function(products){
+           let placeholder = document.querySelector("#section-box-data");
+           let out = "";
+           for(let item of products){
+        
+              out += `
+                      <div class="box-data">
+                      <div id="${item.id}" class="footer-box">
+                      <img src="${item.img}" alt="${item.description}">
+                      </div>
+                      <div class="info-activity">
+                      <div class="title-and-option">
+                      <p class="title">${item.title}</p>
+                      <img src="images/icon-ellipsis.svg">
+                      </div>
+                      <div class="current-previous-div">
+                      <p class="current-hour">${item.timeframes.monthly.current}</p>
+                      <p class="previous-hour">${item.timeframes.monthly.previous}</p>
+                      </div>
+                      </div>
+                      </div>
+                      `;
+        
+           }
+         
+           placeholder.innerHTML = out;
+        });
+        
+        })
